c5aae784cbfef2453a91e6144afc28ff1ff15358
ramin <raminjafary1993@gmail.com>
2020-09-29 00:33:24 +0330

    feat: add examples

---
+++ example/components/title.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>I am cached...</h1>
+</template>
+<script>
+export default {}
+</script>
--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    IT WORKS
-    <cache :items="{ id: 3, last_updated: '2020-09-30' }">
-      <span>cacheble content</span>
+    <cache :cache="{ id: 'title', lastUpdated: '9/29/2020, 12:04:57 AM' }">
+      <Title />
+    </cache>
+    <cache :cache="{ id: 'dummyText', lastUpdated: '9/29/2020, 12:02:36 AM' }">
+      this is going to be cached..!
     </cache>
   </div>
 </template>
 
 <script>
+import Title from '../components/title.vue'
 export default {
+  components: {
+    Title
+  },
   mounted () {
     this.$cache.log()
   }
